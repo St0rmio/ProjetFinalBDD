149f6880f33052be4639d8ed9eca93e7c1da71a4
LucF <123591576+LucFR95@users.noreply.github.com>
2024-04-25 15:37:50 -0400

    Ajouts des tests pour les paramètres dans l'exo 1

--- exercice_01.sql
+++ exercice_01.sql
@@ -20,77 +20,81 @@ BEGIN
     DECLARE j INT DEFAULT 1;
     DECLARE k INT DEFAULT 1;
     
-    -- Création d'un nouvelle université dans la table universite.
-    INSERT INTO universite (nom_universite, sigle, numero_civique, nom_rue, ville, province, code_postal)
-    VALUES (param_nom_universite, param_sigle, param_numero_civique, param_nom_rue, param_ville, param_province, param_code_postal);
-    
-    -- Récupération de l'identifiant de la nouvelle université.
-    SET id_universite = LAST_INSERT_ID();
-    
-    -- Création d'un espace de stationnement de la nouvelle université.
-    INSERT INTO espace_stationnement (designation_espace_stationnement, id_universite)
-    VALUES (CONCAT('Parking de', ' ', param_sigle), id_universite);
-    
-    -- Récupération de l'identifiant de l'espace de stationnement.
-    SET id_espace_stationnement = LAST_INSERT_ID();
-    
-    -- Boucle while pour créer 3 nouvelles allées au nouvel espace de stationnement.
-    WHILE i <= 3 DO
-        -- Création d'une nouvelle allée.
-		INSERT INTO allee (id_espace_stationnement, designation_allee, sens_circulation, nombre_places_dispo, tarif_horaire)
-        VALUES (id_espace_stationnement, CONCAT('Allee ', i), CASE i
-																			WHEN 1 THEN 'Entrée'
-                                                                            WHEN 2 THEN 'Sortie'
-                                                                            WHEN 3 THEN 'Bidirectionnel'
-																		END, 10, 4.5);
-		set i = i + 1;
-    END WHILE;
-    
-    -- Création des places dans les différentes allées.
-    WHILE j <= 3 DO
-		SELECT allee.id_allee INTO id_allee FROM allee WHERE allee.id_espace_stationnement = id_espace_stationnement AND allee.designation_allee = CONCAT('Allee ', j);
-        WHILE k <= 2 DO
-            -- Creation d'une place pour personne à mobilité réduite.
-            INSERT INTO place (type_de_place, id_allee, disponibilite)
-            VALUES ('personnes à mobilité réduite', id_allee, 'oui');
-            
-            set k = k + 1;
-        END WHILE;
-        set k = 1;
-        
-        WHILE k <= 2 DO
-			-- Récupération de l'identifiant de la prochaine place.
-			SELECT IFNULL(MAX(place.id_place) + 1, 1) INTO id_place FROM place;
-            
-            -- Creation d'une place pour véhicules électriques.
-            INSERT INTO place (type_de_place, id_allee, disponibilite)
-            VALUES ('véhicules électriques', id_allee, 'oui');
-            
-            set k = k + 1;
-        END WHILE;
-        set k = 1;
-        
-        WHILE k <= 6 DO
-            -- Creation d'une place standard.
-            INSERT INTO place (type_de_place, id_allee, disponibilite)
-            VALUES ('standard', id_allee, 'oui');
-            
-            set k = k + 1;
-        END WHILE;
-        
-        set k = 1;
-        set j = j + 1;
-    END WHILE;
-    
+    IF param_nom_universite = '' OR param_sigle = '' OR param_numero_civique = '' OR param_nom_rue = '' OR param_ville = '' OR param_code_postal = '' THEN
+		SELECT 'Tous les champs doivent être remplis.' AS message_erreur;
+	ELSEIF EXISTS (SELECT * FROM universite WHERE universite.nom_universite = param_nom_universite AND universite.sigle = param_sigle) THEN
+		SELECT 'L''université existe déjà.' AS message_erreur;
+	ELSE
+		-- Création d'un nouvelle université dans la table universite.
+		INSERT INTO universite (nom_universite, sigle, numero_civique, nom_rue, ville, province, code_postal)
+		VALUES (param_nom_universite, param_sigle, param_numero_civique, param_nom_rue, param_ville, param_province, param_code_postal);
+		
+		-- Récupération de l'identifiant de la nouvelle université.
+		SET id_universite = LAST_INSERT_ID();
+		
+		-- Création d'un espace de stationnement de la nouvelle université.
+		INSERT INTO espace_stationnement (designation_espace_stationnement, id_universite)
+		VALUES (CONCAT('Parking de', ' ', param_sigle), id_universite);
+		
+		-- Récupération de l'identifiant de l'espace de stationnement.
+		SET id_espace_stationnement = LAST_INSERT_ID();
+		
+		-- Boucle while pour créer 3 nouvelles allées au nouvel espace de stationnement.
+		WHILE i <= 3 DO
+			-- Création d'une nouvelle allée.
+			INSERT INTO allee (id_espace_stationnement, designation_allee, sens_circulation, nombre_places_dispo, tarif_horaire)
+			VALUES (id_espace_stationnement, CONCAT('Allee ', i), CASE i
+																				WHEN 1 THEN 'Entrée'
+																				WHEN 2 THEN 'Sortie'
+																				WHEN 3 THEN 'Bidirectionnel'
+																			END, 10, 4.5);
+			set i = i + 1;
+		END WHILE;
+		
+		-- Création des places dans les différentes allées.
+		WHILE j <= 3 DO
+			SELECT allee.id_allee INTO id_allee FROM allee WHERE allee.id_espace_stationnement = id_espace_stationnement AND allee.designation_allee = CONCAT('Allee ', j);
+			WHILE k <= 2 DO
+				-- Creation d'une place pour personne à mobilité réduite.
+				INSERT INTO place (type_de_place, id_allee, disponibilite)
+				VALUES ('personnes à mobilité réduite', id_allee, 'oui');
+				
+				set k = k + 1;
+			END WHILE;
+			set k = 1;
+			
+			WHILE k <= 2 DO
+				-- Récupération de l'identifiant de la prochaine place.
+				SELECT IFNULL(MAX(place.id_place) + 1, 1) INTO id_place FROM place;
+				
+				-- Creation d'une place pour véhicules électriques.
+				INSERT INTO place (type_de_place, id_allee, disponibilite)
+				VALUES ('véhicules électriques', id_allee, 'oui');
+				
+				set k = k + 1;
+			END WHILE;
+			set k = 1;
+			
+			WHILE k <= 6 DO
+				-- Creation d'une place standard.
+				INSERT INTO place (type_de_place, id_allee, disponibilite)
+				VALUES ('standard', id_allee, 'oui');
+				
+				set k = k + 1;
+			END WHILE;
+			
+			set k = 1;
+			set j = j + 1;
+		END WHILE;
+	END IF;
 END $
 DELIMITER ;
-
 
 DELIMITER $
 CREATE TABLE IF NOT EXISTS log_aire_stationnement (id_log INT(11) AUTO_INCREMENT PRIMARY KEY, nom_universite VARCHAR(45), sigle_universite VARCHAR(10), date_heure_tentative DATETIME) $
 DROP TRIGGER IF EXISTS log_creation_espace_stationnement $
 CREATE TRIGGER log_creation_espace_stationnement
-AFTER INSERT ON espace_stationnement
+BEFORE INSERT ON espace_stationnement
 FOR EACH ROW
 BEGIN
 	DECLARE nom_universite VARCHAR(45);
